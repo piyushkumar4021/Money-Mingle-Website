0ab3b73d423ef60d8aa441f9ba828df34e1f10e5
piyushkumar4021 <piyushkumar4075@gmail.com>
2024-06-07 00:40:05 +0530

    Added sticky bar, nav links animation and images lazy loading

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const popupBlur = document.querySelector(".popup__blur");
 
 const sectionFeatures = document.querySelector(".section-features");
 
+const nav = document.querySelector(".nav");
+
 function hidePopup() {
   popupWindow.classList.add("hidden");
   popupBlur.classList.add("hidden");
@@ -90,4 +92,93 @@ document.querySelector(".nav__links").addEventListener("click", (e) => {
   }
 });
 
+////////// Header footer nav hover effect
+
+// header and footer nav links
+const headerFooterNavLinks = document.querySelectorAll(".nav__links");
+
+const navHover = function (e) {
+  if (!e.target.classList.contains("nav__link")) return;
+
+  headerFooterNavLinks.forEach((navLinks) => {
+    navLinks.querySelectorAll(".nav__link").forEach((link) => {
+      if (link !== e.target) link.style.opacity = this;
+    });
+  });
+};
+
+headerFooterNavLinks.forEach((l) =>
+  l.addEventListener("mouseover", navHover.bind("0.5"))
+);
+
+headerFooterNavLinks.forEach((l) =>
+  l.addEventListener("mouseout", navHover.bind("1"))
+);
+
 //
+
+// Sticky nav bar
+const stickyNav = function (entries) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) nav.classList.add("sticky");
+    else nav.classList.remove("sticky");
+  });
+};
+
+const navObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${getComputedStyle(nav).height}`,
+});
+
+navObserver.observe(document.querySelector(".header"));
+
+// Scrolling sections effect
+const sections = document.querySelectorAll("section");
+
+const sectionAnimate = (entries, observer) => {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("hide-section");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(sectionAnimate, {
+  root: null,
+  threshold: 0.15,
+});
+
+sections.forEach((section) => {
+  sectionObserver.observe(section);
+  section.classList.add("hide-section");
+});
+
+// Images lazy loading
+const featureImgs = document.querySelectorAll(".feature__img");
+
+const removeLazyLoading = (entries, observer) => {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener("load", () => {
+    entry.target.classList.remove("lazy-loading");
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const lazyLoading = new IntersectionObserver(removeLazyLoading, {
+  root: null,
+  threshold: 0,
+  rootMargin: "-100px",
+});
+
+featureImgs.forEach((img) => {
+  lazyLoading.observe(img);
+  img.classList.add("lazy-loading");
+});
